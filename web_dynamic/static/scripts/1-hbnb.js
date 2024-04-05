f37aae4cf0519a665cfe59cb8bf1e430bcfd4afe
GeorgeGE7 <elgeo.e.ge.7@gmail.com>
2024-04-05 21:16:45 +0200

    Test

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,16 +1,16 @@
-$(document).ready(function() {
-  $('input[type="checkbox"]').on('change', function() {
-    let amenities = [];
-    $('input[type="checkbox"]:checked').each(function() {
-      amenities.push($(this).data('id'))
-    });
-    $('input[type="checkbox"]:not(:checked)').each(function() {
-      const deleteAm = amenities.indexOf($(this).data('id'))
-      amenities.splice(deleteAm, 1);
-    });
-
-    let amenitiesList = amenities.join(', ');
-    $('.amenities h4').text(amenitiesList);
+$(document).ready(function () {
+  let amenitiesObj = {};
+  $(document).on("change", "input[type='checkbox']", function () {
+    if (this.checked) {
+      amenitiesObj[$(this).data("id")] = $(this).data("name");
+    } else {
+      delete amenitiesObj[$(this).data("id")];
+    }
+    const amenitiesObjValues = Object.values(amenitiesObj);
+    if (amenitiesObjValues?.length > 0) {
+      $("div.amenities > h4").text(Object.values(amenitiesObj).join(", "));
+    } else {
+      $("div.amenities > h4").html("&nbsp;");
+    }
   });
 });
-
